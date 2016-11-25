45d067410288bcfb57d27ccd6d597f99a5623f16
ThuF <jordan.p.pavlov@gmail.com>
2016-11-25 23:41:34 +0200

    Adopts support for open in new tab

--- sample_launchpad_github/ScriptingServices/launchpad/extension/github.js
+++ sample_launchpad_github/ScriptingServices/launchpad/extension/github.js
@@ -2,18 +2,19 @@
 
 exports.getMenuItem = function() {
 	return {
-		name:"GitHub",
-		link:"https://github.com/dirigiblelabs/"
+		name: 'GitHub',
+		link: 'https://github.com/dirigiblelabs/',
+		newTab: true
 	};
 };
 
 exports.getHomeItem = function(){
 	return {
-		image: "github",
-		color: "blue",
-		path: "https://github.com/dirigiblelabs/",
-		title: "GitHub",
-		description: "Dirigible Labs",
+		image: 'github',
+		color: 'blue',
+		path: 'https://github.com/dirigiblelabs/',
+		title: 'GitHub',
+		description: 'Dirigible Labs',
 		newTab: true
 	};
 };
