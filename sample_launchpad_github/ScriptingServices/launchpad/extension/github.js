2425b5c07dc5c64a8a2594156e1e75d5e3cd325c
ThuF <jordan.p.pavlov@gmail.com>
2016-11-25 23:20:28 +0200

    Initial Commit

---
+++ sample_launchpad_github/ScriptingServices/launchpad/extension/github.js
@@ -0,0 +1,19 @@
+/* eslint-env node, dirigible */
+
+exports.getMenuItem = function() {
+	return {
+		name:"GitHub",
+		link:"https://github.com/dirigiblelabs/"
+	};
+};
+
+exports.getHomeItem = function(){
+	return {
+		image: "github",
+		color: "blue",
+		path: "https://github.com/dirigiblelabs/",
+		title: "GitHub",
+		description: "Dirigible Labs",
+		newTab: true
+	};
+};
